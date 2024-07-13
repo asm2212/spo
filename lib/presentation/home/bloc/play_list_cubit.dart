bb5b7627458d93de90461885d090edd49bc7a49f
asm2212 <asmareadmasu0@gmail.com>
2024-07-13 15:15:43 +0300

    play list cubit and  state

--- lib/presentation/home/bloc/play_list_cubit.dart
+++ lib/presentation/home/bloc/play_list_cubit.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../domain/usecases/song/get_play_list.dart';
-import '../../../service_locator.dart';
-import 'play_list_state.dart';
+import 'package:spo/domain/usecases/song/get_play_list.dart';
+import 'package:spo/presentation/home/bloc/play_list_state.dart';
+import 'package:spo/service_locator.dart';
+
 
 class PlayListCubit extends Cubit<PlayListState> {
 
