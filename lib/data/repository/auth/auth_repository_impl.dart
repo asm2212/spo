8eb898c55acd0912a33a472980f3c49d3497f7e5
asm2212 <asmareadmasu0@gmail.com>
2024-07-13 15:38:33 +0300

    repo

--- lib/data/repository/auth/auth_repository_impl.dart
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
-import 'package:spotify/data/models/auth/create_user_req.dart';
-import 'package:spotify/data/models/auth/signin_user_req.dart';
-import 'package:spotify/data/sources/auth/auth_firebase_service.dart';
-import 'package:spotify/domain/repository/auth/auth.dart';
+import 'package:spo/data/models/auth/create_user_req.dart';
+import 'package:spo/data/models/auth/signin_user_req.dart';
+import 'package:spo/data/sources/auth/auth_firebase_service.dart';
+import 'package:spo/domain/repository/auth/auth.dart';
+import 'package:spo/service_locator.dart';
 
-import '../../../service_locator.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
 
--- lib/data/repository/song/song_repository_impl.dart
+++ lib/data/repository/song/song_repository_impl.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:spotify/data/sources/song/song_firebase_service.dart';
-import 'package:spotify/domain/repository/song/song.dart';
+import 'package:spo/data/sources/song/song_firebase_service.dart';
+import 'package:spo/domain/repository/song/song.dart';
+import 'package:spo/service_locator.dart';
 
-import '../../../service_locator.dart';
 
 class SongRepositoryImpl extends SongsRepository {
 
