dd6253c7ef4127a20a5d19ff610f269f0b1cb7cd
asm2212 <asmareadmasu0@gmail.com>
2024-07-13 15:22:55 +0300

    signin or signup pages

--- lib/presentation/auth/pages/signup_or_siginin.dart
+++ lib/presentation/auth/pages/signup_or_siginin.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:spotify/common/helpers/is_dark_mode.dart';
-import 'package:spotify/common/widgets/button/basic_app_button.dart';
-import 'package:spotify/core/configs/assets/app_images.dart';
-import 'package:spotify/core/configs/assets/app_vectors.dart';
-import 'package:spotify/core/configs/theme/app_colors.dart';
-import 'package:spotify/presentation/auth/pages/signin.dart';
-import 'package:spotify/presentation/auth/pages/signup.dart';
-
-import '../../../common/widgets/appbar/app_bar.dart';
-
+import 'package:spo/common/helpers/is_dark_mode.dart';
+import 'package:spo/common/widgets/appbar/app_bar.dart';
+import 'package:spo/common/widgets/button/basic_app_button.dart';
+import 'package:spo/core/configs/assets/app_images.dart';
+import 'package:spo/core/configs/assets/app_vectors.dart';
+import 'package:spo/core/configs/theme/app_colors.dart';
+import 'package:spo/presentation/auth/pages/signin.dart';
+import 'package:spo/presentation/auth/pages/signup.dart';
 class SignupOrSigninPage extends StatelessWidget {
   const SignupOrSigninPage({super.key});
 
