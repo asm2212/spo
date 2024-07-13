7a66996572c126c251d607c42b89d2dddec38078
asm2212 <asmareadmasu0@gmail.com>
2024-07-13 14:52:54 +0300

    service locate

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,14 +1,15 @@
 import 'package:get_it/get_it.dart';
-import 'package:spotify/data/repository/auth/auth_repository_impl.dart';
-import 'package:spotify/data/sources/auth/auth_firebase_service.dart';
-import 'package:spotify/domain/repository/auth/auth.dart';
-import 'package:spotify/domain/usecases/auth/get_user.dart';
-import 'package:spotify/domain/usecases/auth/signup.dart';
-import 'package:spotify/domain/usecases/song/add_or_remove_favorite_song.dart';
-import 'package:spotify/domain/usecases/song/get_favorite_songs.dart';
-import 'package:spotify/domain/usecases/song/get_news_songs.dart';
-import 'package:spotify/domain/usecases/song/get_play_list.dart';
-import 'package:spotify/domain/usecases/song/is_favorite_song.dart';
+import 'package:spo/data/repository/auth/auth_repository_impl.dart';
+import 'package:spo/data/sources/auth/auth_firebase_service.dart';
+import 'package:spo/domain/repository/auth/auth.dart';
+import 'package:spo/domain/usecases/auth/get_user.dart';
+import 'package:spo/domain/usecases/auth/signup.dart';
+import 'package:spo/domain/usecases/song/add_or_remove_favorite_song.dart';
+import 'package:spo/domain/usecases/song/get_favorite_songs.dart';
+import 'package:spo/domain/usecases/song/get_news_songs.dart';
+import 'package:spo/domain/usecases/song/get_play_list.dart';
+import 'package:spo/domain/usecases/song/is_favorite_song.dart';
+
 
 import 'data/repository/song/song_repository_impl.dart';
 import 'data/sources/song/song_firebase_service.dart';
@@ -30,11 +31,11 @@ Future<void> initializeDependencies() async {
  
 
  sl.registerSingleton<AuthRepository>(
-  AuthRepositoryImpl()
+  AuthRepositoryImpl() as AuthRepository
  );
 
  sl.registerSingleton<SongsRepository>(
-  SongRepositoryImpl()
+  SongRepositoryImpl() as SongsRepository
  );
 
 
