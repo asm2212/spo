c462cc04931b6005c23d576be0bdbe580e97acc4
asm2212 <asmareadmasu0@gmail.com>
2024-07-13 15:14:04 +0300

    home

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:spotify/common/helpers/is_dark_mode.dart';
-import 'package:spotify/core/configs/assets/app_images.dart';
-import 'package:spotify/core/configs/theme/app_colors.dart';
-import 'package:spotify/presentation/home/widgets/news_songs.dart';
-import 'package:spotify/presentation/home/widgets/play_list.dart';
-import 'package:spotify/presentation/profile/pages/profile.dart';
+import 'package:spo/common/helpers/is_dark_mode.dart';
+import 'package:spo/common/widgets/appbar/app_bar.dart';
+import 'package:spo/core/configs/assets/app_images.dart';
+import 'package:spo/core/configs/assets/app_vectors.dart';
+import 'package:spo/core/configs/theme/app_colors.dart';
+import 'package:spo/presentation/home/widgets/news_songs.dart';
+import 'package:spo/presentation/home/widgets/play_list.dart';
+import 'package:spo/presentation/profile/pages/profile.dart';
 
-import '../../../common/widgets/appbar/app_bar.dart';
-import '../../../core/configs/assets/app_vectors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
